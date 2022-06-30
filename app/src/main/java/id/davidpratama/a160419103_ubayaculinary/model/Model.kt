58ff0db6049943b09a1b060abc79e8142293c651
s160419006 <s160419006@student.ubaya.ac.id>
2022-06-30 16:43:05 +0700

    test

--- app/src/main/java/id/davidpratama/a160419103_ubayaculinary/model/Model.kt
+++ app/src/main/java/id/davidpratama/a160419103_ubayaculinary/model/Model.kt
@@ -5,6 +5,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+//test
 @Entity
 data class Restaurant(
     @ColumnInfo(name = "name")
